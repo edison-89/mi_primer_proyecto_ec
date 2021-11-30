93197bcdf9501a6ee7936291b41cc0b21a0c5d08
edison <edison@10.1.144.178>
2021-11-29 20:04:50 -0500

    El primer proyecto que hicimos en clase con GitHub

---
+++ src/main/java/mi/primer/proyecto/ec/Animal.java
@@ -0,0 +1,10 @@
+package mi.primer.proyecto.ec;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mi primer proyecto con GitHub");
+	}
+
+}
