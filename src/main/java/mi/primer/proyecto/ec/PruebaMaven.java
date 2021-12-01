54787c3ea3c3e9532850380201909a8b2a855df5
edison <edison@10.1.145.193>
2021-11-30 20:14:10 -0500

    Mi primer proyecto maven

---
+++ src/main/java/mi/primer/proyecto/ec/PruebaMaven.java
@@ -0,0 +1,10 @@
+package mi.primer.proyecto.ec;
+
+public class PruebaMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mi primer proyecto maven");
+	}
+
+}
