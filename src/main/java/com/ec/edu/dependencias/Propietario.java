5db1a33fb99aa235d38fbc17be321f04662b22f2
Edison Cayambe <edisonc_89@hotmail.com>
2022-01-24 19:34:51 -0500

    test

---
+++ src/main/java/com/ec/edu/dependencias/Propietario.java
@@ -0,0 +1,27 @@
+package com.ec.edu.dependencias;
+
+public class Propietario {
+	private String nombre;
+	private String apellido;
+	private int edad;
+	
+	//Metodos SET Y GET
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+}
