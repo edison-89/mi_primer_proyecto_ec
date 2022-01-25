8c43b2f999c6e09e5c420beb570889fe462e543c
Edison Cayambe <edisonc_89@hotmail.com>
2022-01-24 19:35:01 -0500

    r

---
+++ src/main/java/com/ec/edu/dependencias/Vehiculo.java
@@ -0,0 +1,48 @@
+package com.ec.edu.dependencias;
+
+public class Vehiculo {
+	protected String marca;
+	protected String modelo;
+	protected String tipo;
+	protected Propietario propietario;
+	protected BigDecimal precio;
+	protected BigDecimal valorMatricula;
+	
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public Propietario getPropietario() {
+		return propietario;
+	}
+	public void setPropietario(Propietario propietario) {
+		this.propietario = propietario;
+	}
+	public BigDecimal getPrecio() {
+		return precio;
+	}
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
+	}
+	public BigDecimal getValorMatricula() {
+		return valorMatricula;
+	}
+	public void setValorMatricula(BigDecimal valorMatricula) {
+		this.valorMatricula = valorMatricula;
+	}
+}
